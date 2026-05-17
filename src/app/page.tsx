5ec3bd7e9be212e3e039c788198e3ed0f953b24c
Marcio Dutra <professormarciodutra@gmail.com>
2026-05-17 19:16:56 -0300

    Definindo tipagem para produto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,33 @@
+import { ProductType } from "@/types/ProductType";
 
+async function getProducts() {
+  const res = await fetch('https://fakestoreapi.com/products');
 
-export default function Home() {
+  if (!res.ok) {
+    throw new Error('Failed to fetch products');
+  }
+
+  return res.json();
+}
+
+export default async function Home() {
+  const products = await getProducts();
+
+  console.log(products);
+
   return (
-    <div className="max-w-7xl mx-auto pt-8 px-8 xl=px=0">
+    <div className="max-w-7xl mx-auto pt-8 px-8 xl:px-0">
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 xl:gap-6">
 
-        <div className="bg-slate-800 rounded-lg p-4">prod 1</div>
-        <div className="bg-slate-800 rounded-lg p-4">prod 1</div>
-        <div className="bg-slate-800 rounded-lg p-4">prod 1</div>
-        <div className="bg-slate-800 rounded-lg p-4">prod 1</div>
+        {products.map((product: ProductType) => (
+          <div key={product.id}>
+            {product.title}
+          </div>
+        ))}
 
       </div>
+
       <h1>Next E-commerce!</h1>
     </div>
-  )
+  );
 }
