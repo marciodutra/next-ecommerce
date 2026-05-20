245eefcb49d335d0e2e2c1b5f6c541f423a4fa5f
Marcio Dutra <professormarciodutra@gmail.com>
2026-05-20 15:02:24 -0300

    Parte dos produtos do carrinho

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -6,10 +6,9 @@ import {
   SignInButton
 } from "@clerk/nextjs";
 import { use } from 'react';
-import { useCartStore } from '@/store';
+import Cart from './Cart';
 
-function Navbar() {
-  // const useSotre = useCartStore();
+function Navbar() {  
 
   return (
     <nav className="fixed top-0 left-0 w-full h-14 flex items-center px-8 justify-between z-50 bg-slate-800 text-gray-300">
@@ -21,26 +20,7 @@ function Navbar() {
       </Link>
 
       <div className='flex items-center gap-8'>
-        <div className='flex items-center cursor-pointer relative'>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-5 w-5"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-            />
-          </svg>
-          <span className='bg-teal-600 text-sm font-bold rounded-full h-5 w-5 flex items-center 
-                           justify-center absolute left-3 bottom-3'>
-            2
-          </span>
-        </div>
+          <Cart />
         <div>
           <SignedIn>
             <UserButton />
