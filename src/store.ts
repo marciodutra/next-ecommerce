f29210bfb193bde215a48ca1662e6812ce46d278
Marcio Dutra <professormarciodutra@gmail.com>
2026-05-20 16:10:03 -0300

    Botão para adicionar itens ao carrinho

--- src/store.ts
+++ src/store.ts
@@ -4,15 +4,46 @@ import { ProductType } from '@/types/ProductType';
 
 type CartState = {
     cart: ProductType[];
-    // addToCart: (product: ProductType) => void;
-    // removeFromCart: (productId: number) => void;
+    addProduct: (product: ProductType) => void;
+    // removeProduct: (productId: number) => void;
     isOpen: boolean;
     toogleCart: () => void;
-}    
+}
+
 export const useCartStore = create<CartState>()(
-    persist((set) => ({
-        cart: [],
-        isOpen: false,
-        toogleCart: () => set((state) => ({ isOpen: !state.isOpen })),
-    }), { name: 'cart-storage'} )
-)
+    persist(
+        (set) => ({
+            cart: [],
+            addProduct: (product) =>
+                set((state) => {
+                    const existingProduct = state.cart.find(
+                        (p) => p.id === product.id
+                    );
+
+                    if (existingProduct) {
+                        const updatedCart = state.cart.map((p) => {
+                            if (p.id === product.id) {
+                                return { ...p, quantity: +1 };
+                            }
+
+                            return p;
+                        });
+
+                        return { cart: updatedCart };
+                    } else {
+                        return {
+                            cart: [
+                                ...state.cart,
+                                { ...product, quantity: 1 }
+                            ]
+                        };
+                    }
+                }),
+
+            isOpen: false,
+            toogleCart: () =>
+                set((state) => ({ isOpen: !state.isOpen })),
+        }),
+        { name: 'cart-storage' }
+    )
+);
