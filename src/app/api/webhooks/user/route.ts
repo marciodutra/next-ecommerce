84e90a9f1518673db85c19e9b2912b73c80dd3d0
Marcio Dutra <professormarciodutra@gmail.com>
2026-05-21 12:28:05 -0300

    Adicionando usuário no stripe

--- src/app/api/webhooks/user/route.ts
+++ src/app/api/webhooks/user/route.ts
@@ -3,6 +3,7 @@ import { IncomingHttpHeaders } from "http";
 import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 import { Webhook, WebhookRequiredHeaders } from "svix";
+import Stripe from "stripe";
 
 const webhookSecret = process.env.CLERK_WEBHOOK_SECRET || '';
 
@@ -75,13 +76,24 @@ async function handler(request: Request) {
             ...attributes
         } = evt.data;
 
+            const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+              apiVersion: "2026-04-22.dahlia",
+            });
+
+            const customer = await stripe.customers.create({
+                name: `${first_name} ${last_name}`,
+                email: email_addresses ? email_addresses[0].email_address : '',
+            })
+
         await prisma.user.upsert({
             where: {
-                externalId: id as string
+                externalId: id as string,               
+                
             },
 
             create: {
                 externalId: id as string,
+                striperCustomerId: customer.id,
                 attributes
             },
 
